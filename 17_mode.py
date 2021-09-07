c6fcd5b426a59cb89f2155ab596e1a309361517f
kimberly lin <kimberly.lin10@gmail.com>
2021-09-07 12:20:14 -0700

    15-17

--- 17_mode.py
+++ 17_mode.py
@@ -11,3 +11,10 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    (highestNum,count)=(0,0)
+    uniqueNum= set(nums)
+    for num in uniqueNum:
+        if nums.count(num) > count:
+            count = nums.count(num)
+            highestNum = num
+    return highestNum
